772563b4ddaa7d3b0726312fe2536d9bada8456d
papaya <1643700595@qq.com>
2018-05-18 20:57:07 +0800

    add difference

--- wpfalive-lodash.js
+++ wpfalive-lodash.js
@@ -4,11 +4,54 @@
 
 const wpfalive = {}
 
-wpfalive.chunk = function(array, size=1) {
+wpfalive.chunk = function (array, size=1) {
     const result = []
     for(let i = 0; i < array.length; i += size) {
         result.push(array.slice(i, i + size))
     }
     return result
 }
+
+/**
+ * Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey
+ */
+wpfalive.compact = function(array) {
+    return array.filter(item => !!item)
+}
+
+wpfalive.concat = function(array, ...vals) {
+    return Array.prototype.concat.apply(array, vals)
+}
+
+/**
+ * Similar to same-value equality, but considered +0 and -0 equal.
+ * same-value equality认为+0和-0相等
+ * [difference description]
+ * _.difference([2, 1], [2, 3]) => [1]
+ * difference([2, 1, 3], [2, 3], [3, 4]) => [1]
+ */
+wpfalive.difference = function(array, ...vals) {
+    const result = []
+    // 合并vals中所有array
+    const totalAry = vals.reduce((a, b) => {
+        return a = a.concat(b)
+    })
+
+    // 对合并的数组去重
+    const mergedAry = [...new Set(totalAry)]
+    
+    return array.filter(it => {
+        return !mergedAry.includes(it)
+    })
+}
+
+
+
+
+
+
+
+
+
+
 
